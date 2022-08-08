ff2a019c85c0a1474bb677a3bf395605582f1c4a
David Hill <dhill@mindcry.org>
2022-08-08 16:57:02 -0400

    build: Test against Go 1.19

--- breacharbiter_test.go
+++ breacharbiter_test.go
@@ -523,10 +523,7 @@ func copyRetInfo(retInfo *retributionInfo) *retributionInfo {
 		breachHeight:    retInfo.breachHeight,
 		breachedOutputs: make([]breachedOutput, nOutputs),
 	}
-
-	for i := range retInfo.breachedOutputs {
-		ret.breachedOutputs[i] = retInfo.breachedOutputs[i]
-	}
+	copy(ret.breachedOutputs, retInfo.breachedOutputs)
 
 	return ret
 }
